294c3b4996339d9289b99e950d8f6a2cb344eb3d
Dat Ng <laohac83x@gmail.com>
2019-06-25 16:41:23 +0700

    update property

--- Sources/ClaimSet.swift
+++ Sources/ClaimSet.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public struct ClaimSet {
-  var claims: [String: Any]
+  public private(set) var claims: [String: Any]
 
   public init(claims: [String: Any]? = nil) {
     self.claims = claims ?? [:]
